fe3643aab807dc0fb6cdc1b64dc6f9f87069283c
Pranavi <pranavibitra97@gmail.com>
2019-10-06 16:13:26 +0200

    changed modifiers order again

--- inception-layer-docmetadata/src/main/java/de/tudarmstadt/ukp/inception/ui/core/docanno/sidebar/DocumentMetadataAnnotationDetailPanel.java
+++ inception-layer-docmetadata/src/main/java/de/tudarmstadt/ukp/inception/ui/core/docanno/sidebar/DocumentMetadataAnnotationDetailPanel.java
@@ -398,7 +398,7 @@ public class DocumentMetadataAnnotationDetailPanel extends Panel
     {
         private static final long serialVersionUID = 1L;
 
-        public final static IsSidebarAction INSTANCE = new IsSidebarAction();
+        public static final IsSidebarAction INSTANCE = new IsSidebarAction();
     }
     
     public void toggleVisibility()
